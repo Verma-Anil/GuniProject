e38ef9d749e8d744d004616b8b58e259a1b7ada3
vanil3219@gmail.com <vanil3219@gmail.com>
2022-03-18 17:20:36 +0530

    added the scafloded customize the user and role

---
+++ GuniThises.web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GuniThises.web.Data;
+using GuniThises.web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuniThises.web.Areas.Identity.IdentityHostingStartup))]
+namespace GuniThises.web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
